f655c1d2b630a6a812ef42afec38f6fd9fae83f7
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2025-01-30 23:43:43 +0100

    Fix syntax

--- function.js
+++ function.js
@@ -1,61 +1,61 @@
 function createfield() {
  n = $('#my_input').val();
  $("#matrix").html('');
- if (n <= 20 && n >= 2){
+ if (n <= 20 && n >= 2) {
 
-   $("#matrix").html('');
-   $("#matrix").append('<h2>Bitte Adjazenmatrix(A)(Verbindungsmatrix) eingeben</h2>')
+  $("#matrix").html('');
+  $("#matrix").append('<h2>Bitte Adjazenmatrix(A)(Verbindungsmatrix) eingeben</h2>')
 
-   //$("#matrix").append('<form id="matrixinput" method="post" onsubmit="return false"><br>');
+  //$("#matrix").append('<form id="matrixinput" method="post" onsubmit="return false"><br>');
 
-   var inputs='<form id="matrix" action="../response.sh"><br>';
+  var inputs = '<form id="matrix" action="../response.sh"><br>';
 
-   for (var i = 0; i < n; i++) {
-       for (var k = 0; k < n; k++) {
-           inputs += '<input value="0" min="0" type="number" id="r' + i + 'c' + k +'" name="r' + i + 'c' + k +'" style=" margin: auto; width:60px;">';
-       }
-       inputs +='<br>';
+  for (var i = 0; i < n; i++) {
+   for (var k = 0; k < n; k++) {
+    inputs += '<input value="0" min="0" type="number" id="r' + i + 'c' + k + '" name="r' + i + 'c' + k + '" style=" margin: auto; width:60px;">';
    }
-   inputs +='<input id="matrixsubmit" type="submit" value="bestätigen">';
-   inputs +='</form>';
+   inputs += '<br>';
+  }
+  inputs += '<input id="matrixsubmit" type="submit" value="bestätigen">';
+  inputs += '</form>';
 
-   $("#matrix").append(inputs);
- 
- }
- else{
-   $("#matrix").append("Bitte einen n Dimension von unter 2-20 wählen.");
+  $("#matrix").append(inputs);
 
  }
-   /*
-   for (var i = 1; i <= n; i++) {
-     $("#matrix").append('<span>Item ' + i + ' </span><input id="rolo_add' + i + '" name="addposition"  type="text" value="" required/><br>');
-
-   }*/
-
- 
+ else {
+  $("#matrix").append("Bitte einen n Dimension von unter 2-20 wählen.");
 
  }
-
-
  /*
- $('#matrixsubmit').click(function(e){
-   e.preventDefault();
-   n = $('#matrixinput').val();
-   $('#matrixresponse').html('');
-   $('#matrixresponse').append(n);
-   
- })
+ for (var i = 1; i <= n; i++) {
+   $("#matrix").append('<span>Item ' + i + ' </span><input id="rolo_add' + i + '" name="addposition"  type="text" value="" required/><br>');
+
+ }*/
+
+
+
+}
+
+
+/*
+$('#matrixsubmit').click(function(e){
+  e.preventDefault();
+  n = $('#matrixinput').val();
+  $('#matrixresponse').html('');
+  $('#matrixresponse').append(n);
+  
+})
  
 
- function response() {
+function response() {
 
-   
-   n = $('#matrix').val();
-   $('#matrixresponse').html('');
-   $('#matrixresponse').append(n +'Hi there kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
-   
- }
+  
+  n = $('#matrix').val();
+  $('#matrixresponse').html('');
+  $('#matrixresponse').append(n +'Hi there kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
+  
+}
  
 */
- 
- 
+
+
